b712d30d77c823d7149dc893a6aa006c2dc91582
Immaridel <cerbdog@gmail.com>
2022-10-14 19:52:05 -0700

    Update Character Script.ps1
    Added modification to EQLogDir and created EQDir with errorhandling to prevent the script from starting if EQ is not running.

--- Character Script.ps1
+++ Character Script.ps1
@@ -3,7 +3,13 @@
 Remove-Variable * -ErrorAction SilentlyContinue
 $PSDefaultParameterValues = @{ '*:Encoding' = 'utf8' }
 #=========Variables (Change these)=========
-$EQLogDir = "E:\Games\P99\Logs"
+$EQDir = Split-Path -Path (Get-Process -Name eqgame -ErrorVariable EQError).path
+if ($EQError -ne 0) {
+    throw "Everquest is not running.  Please start the game and try again."
+} else {
+    Write-Host "Everquest is not running.  Please start the game and try again."
+    }
+$EQLogDir = "$EQDir\Logs"
 $CheckDelay = 5 #Seconds
 #$LogSplitThreshhold = 10
 
